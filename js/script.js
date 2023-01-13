6d4dd02d6e16da912c9a6820a1cb28011bce9695
Unknick1996x <nickanthony.escobido@gmail.com>
2023-01-14 07:50:40 +0800

    sounds added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button'));
+var audio = new Audio();
+audio.src = "sounds/sounds.wav";
+
+var error = new Audio();
+error.src = "sounds/error.wav";
+
+var success = new Audio();
+success.src = "sounds/success.wav";
+
+var clear = new Audio();
+clear.src = "sounds/clear.wav";
+
+var bclear = new Audio();
+bclear.src = "sounds/bclear.wav";
 
 buttons.map( button => {
     button.addEventListener('click', (e) => {
@@ -7,19 +21,23 @@ buttons.map( button => {
             case 'C':
                 display.innerText = '';
                 display.classList.remove("error-field");
+                clear.play();
                 break;
 
             case '←':
                 if(display.innerText){
                     display.innerText = display.innerText.slice(0,-1);
+                    bclear.play();
                 }
                 break;
             case '=':
                 try{
                     display.innerText = eval(display.innerText);
+                    success.play();
                 } catch {
                     display.innerText = 'SYNTAX ERROR';
                     display.classList.add("error-field");
+                    error.play();
                 }
                 display.innerText = eval(display.innerText);
                 break;
